c863fd7b48661efe14cfe766a1f7882cc75be20a
fancyLv <flqzdzxx@163.com>
2020-04-21 00:56:35 +0800

    爬取安居客全国历史房价

---
+++ L13/Anjuke/Anjuke/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+from scrapy import Field, Item
+
+
+class AnjukeItem(Item):
+    province = Field()
+    city = Field()
+    date = Field()
+    price = Field()
+    compare = Field()
---
+++ L13/Anjuke/Anjuke/spiders/anjuke.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import Request
+import calendar
+import datetime
+from Anjuke.items import AnjukeItem
+
+
+class AnjukeSpider(scrapy.Spider):
+    name = 'anjuke'
+    allowed_domains = ['anjuke.com']
+    start_urls = ['https://www.anjuke.com/fangjia/']
+
+    def parse(self, response):
+        # 首页
+        for url in response.css('.elem-l > a::attr(href)').extract():
+            yield Request(url=url, callback=self.parse_province_list)
+
+    def parse_province_list(self, response):
+        # 各省历年数据列表页
+        for url in response.css('h3 > a::attr(href)').extract():  # 更多
+            yield Request(url, callback=self.parse_city_listl)
+
+    def parse_city_listl(self, response):
+        # 各市列表页
+        for url in response.css('.boxstyle1 ul > li > a::attr(href)').extract():
+            yield Request(url, callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        province = response.css('.crumb > a::text').re('(?:\d+)(.+)(?:房价)')[-1]
+        city = response.css('.fjlist-box h3::text').re('\d+年(.+?)房价')[0]
+        results = response.css('.boxstyle2')
+        for i in results[0].css('ul li'):
+            item = AnjukeItem()
+            year, month = i.css('b').re('\d+')
+            day = calendar.monthrange(int(year), int(month))[1]
+            item['date'] = datetime.datetime(int(year), int(month), day).strftime('%Y-%m-%d')
+            item['price'] = i.css('span').re('[\d\.]+')[0] if i.css('span').re('[\d\.]+') else None
+            item['compare'] = i.css('em::text').extract_first()
+            item['province'] = province
+            item['city'] = city
+            yield item
